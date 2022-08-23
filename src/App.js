4cbf505d986efcf1956bb49376896b7dbfdbb83e
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-08-23 17:32:20 -0400

    Added some JS to parent component and moved some styles to global level

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
+import { useRef } from "react";
+
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
+  const myRef = useRef(null);
+  const myRefProjects = useRef(null);
+  const myRefContact = useRef(null);
+
+  const executeScroll = (scrollToSection) =>
+    scrollToSection.current.scrollIntoView();
+
   return (
     <>
-      <Header />
-      <Body />
-      <Footer />
+      <Header
+        myRef={myRef}
+        myRefProjects={myRefProjects}
+        myRefContact={myRefContact}
+        executeScroll={executeScroll}
+      />
+      <Body myRef={myRef} myRefProjects={myRefProjects} />
+      <Footer myRefContact={myRefContact} />
     </>
   );
 }
