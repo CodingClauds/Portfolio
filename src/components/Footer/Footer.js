e3ed0407b7c145bbc397196dafabd270e0555f2e
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-08-31 16:32:11 -0400

    Updated the footer design with all the respective links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import "./Footer.scss";
 import github from "../../assets/images/social-icons/github-white.svg";
 import linkedin from "../../assets/images/social-icons/linkedin-white.svg";
-import twitter from "../../assets/images/social-icons/twitter-white.svg";
+import email from "../../assets/images/social-icons/email-white.png";
 
 export default function Footer({ myRefContact }) {
   return (
@@ -11,15 +11,9 @@ export default function Footer({ myRefContact }) {
           <h2 className="contact__title">Let's Chat!</h2>
 
           <div className="contact__container-wrapper">
-            <div className="contact__contentbox">
+            {/* <div className="contact__contentbox">
               <p className="contact__text"></p>
-              <a
-                className="contact__email"
-                href="mailto:claudio.000.miranda@gmail.com"
-              >
-                claudio.000.miranda@gmail.com
-              </a>
-            </div>
+            </div> */}
 
             <div className="contact__social">
               <a href="https://www.linkedin.com/in/miranda-claudio/">
@@ -36,11 +30,12 @@ export default function Footer({ myRefContact }) {
                   alt="github icon"
                 />
               </a>
-              <img
-                className="contact__handle"
-                src={twitter}
-                alt="twitter icon"
-              />
+              <a
+                className="contact__email"
+                href="mailto:claudio.000.miranda@gmail.com"
+              >
+                <img className="contact__handle" src={email} alt="email icon" />
+              </a>
             </div>
 
             <div class="site-footer-divider"></div>
