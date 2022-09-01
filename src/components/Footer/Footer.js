613a7428c36ec282c8be284fa17a9906d26f45a8
Claudio Miranda <claudio.000.miranda@gmail.com>
2022-09-01 14:07:45 -0400

    Codebase cleanup

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,43 +8,32 @@ export default function Footer({ myRefContact }) {
     <>
       <section className="contact wrapper" id="contact" ref={myRefContact}>
         <div className="contact__container">
-          <h2 className="contact__title">Let's Chat!</h2>
+          <h2 className="contact__title">let's connect</h2>
+          <div className="contact__social">
+            <a href="https://www.linkedin.com/in/miranda-claudio/">
+              <img
+                className="contact__handle"
+                src={linkedin}
+                alt="linkedin icon"
+              />
+            </a>
+            <a href="https://github.com/CodingClauds">
+              <img className="contact__handle" src={github} alt="github icon" />
+            </a>
+            <a
+              className="contact__email"
+              href="mailto:claudio.000.miranda@gmail.com"
+            >
+              <img className="contact__handle" src={email} alt="email icon" />
+            </a>
+          </div>
 
-          <div className="contact__container-wrapper">
-            {/* <div className="contact__contentbox">
-              <p className="contact__text"></p>
-            </div> */}
+          <div class="site-footer-divider"></div>
 
-            <div className="contact__social">
-              <a href="https://www.linkedin.com/in/miranda-claudio/">
-                <img
-                  className="contact__handle"
-                  src={linkedin}
-                  alt="linkedin icon"
-                />
-              </a>
-              <a href="https://github.com/CodingClauds">
-                <img
-                  className="contact__handle"
-                  src={github}
-                  alt="github icon"
-                />
-              </a>
-              <a
-                className="contact__email"
-                href="mailto:claudio.000.miranda@gmail.com"
-              >
-                <img className="contact__handle" src={email} alt="email icon" />
-              </a>
-            </div>
-
-            <div class="site-footer-divider"></div>
-
-            <div className="contact__acknowledgements">
-              <span className="contact__copyright">
-                Claudio Miranda © All Rights Reserved 2022.
-              </span>
-            </div>
+          <div className="contact__acknowledgements">
+            <span className="contact__copyright">
+              Claudio Miranda © All Rights Reserved 2022.
+            </span>
           </div>
         </div>
       </section>
