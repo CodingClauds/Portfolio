c453333bb95d7139a335f4656a4f1470d00f9f3d
Claudio Miranda <claudio_769@hotmail.com>
2022-10-27 14:40:24 -0400

    Created additional map function and variables for projects

--- src/assets/database/techdb.js
+++ src/assets/database/techdb.js
@@ -1,3 +1,4 @@
+// Techstack assets
 import ReactIcon from "../../assets/images/techstack/react.svg";
 import HtmlIcon from "../../assets/images/techstack/html5.svg";
 import CssIcon from "../../assets/images/techstack/css3.svg";
@@ -8,6 +9,13 @@ import GithubIcon from "../../assets/images/techstack/github.svg";
 import RouterIcon from "../../assets/images/techstack/reactrouter.svg";
 import AxiosIcon from "../../assets/images/techstack/axios.svg";
 import NpmIcon from "../../assets/images/techstack/npm.svg";
+
+// Projects assets
+import Horoscope from "../../assets/images/projects/horoscope.png";
+import Maesurf from "../../assets/images/projects/maesurf.png";
+import Sproutr from "../../assets/images/projects/sproutr.png";
+import F1 from "../../assets/images/projects/f1stopwatch.png";
+import DarkMode from "../../assets/images/projects/dark-light-mode.png";
 
 export const db = [
   { src: ReactIcon },
@@ -20,4 +28,67 @@ export const db = [
   { src: RouterIcon },
   { src: AxiosIcon },
   { src: NpmIcon },
+];
+
+export const projectCard = [
+  {
+    src: Horoscope,
+    textContent: {
+      title: "Horoscoped",
+      description:
+        "For one of our hackathons we created a Horoscope app that utilizes API’s to retrieve data when an individual user inputs their zodiac sign or their specific birthday.",
+    },
+
+    button: {
+      demoURL: "https://hororscoped.netlify.app/",
+      codeURL: "https://github.com/CodingClauds/horoscoped",
+    },
+  },
+  {
+    src: F1,
+    textContent: {
+      title: "F1",
+      description:
+        "Here's the Formula-1 flipcard game with all 20 drivers from the 2022 driver lineup, try to match the drivers with their teams before the time runs out! Let's see if you have what it takes to have a podium finish.",
+    },
+    button: {
+      demoURL: "https://flipcardformula1.netlify.app/",
+      codeURL: "https://github.com/CodingClauds/F1-stopwatch",
+    },
+  },
+  {
+    src: Sproutr,
+    textContent: {
+      title: "Sproutr",
+      description:
+        "Sproutr is a habit building app that lets you choose & track a new or existing habit. At Sproutr we are about helping those create the habits that matter. The approach we decided to take was to create a friendly and visually appealing app to help those making habits fun.",
+    },
+    button: {
+      demoURL: "https://sproutr.netlify.app/",
+      codeURL: "https://github.com/CodingClauds/sproutr-capstone",
+    },
+  },
+  {
+    src: DarkMode,
+    textContent: {
+      title: "DarkMode",
+      description:
+        "For this challenge, I wanted to implement a light and dark mode switch toggle that would change the colours or invert the colours from the default to dark mode for this basic stopwatch component. The method I used was creating variables for the two color states, and adding the toggle on those two throughout.",
+    },
+    button: {
+      demoURL: "https://dark-light-mode-stopwatch.netlify.app/",
+      codeURL: "https://github.com/CodingClauds/Dark-light-Mode-StopWatch",
+    },
+  },
+  {
+    src: Maesurf,
+    textContent: {
+      title: "Maesurf",
+      description:
+        "MaeSurf is a surf camp website that is designed to help travellers book full surf camps and lessons in Costa Rica. Currently tasked with organizing the site and adding in key functionalities to help attract traffic to the site.",
+    },
+    button: {
+      demoURL: "https://maesurf.com/",
+    },
+  },
 ];
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Projects.scss";
 
-import Horoscope from "../../assets/images/projects/horoscope.png";
-import Maesurf from "../../assets/images/projects/maesurf.png";
-import Sproutr from "../../assets/images/projects/sproutr.png";
-import F1 from "../../assets/images/projects/f1stopwatch.png";
-import DarkMode from "../../assets/images/projects/dark-light-mode.png";
+import { projectCard } from "../../assets/database/techdb.js";
 
 export default function Projects() {
   return (
@@ -15,198 +11,45 @@ export default function Projects() {
         <h3 className="projects__title">Click to view my recent work:</h3>
 
         <section className="projects__grid">
-          <div className="project">
-            <div className="content">
-              <div className="content__photocard">
-                <img
-                  className="content__photocard-img"
-                  src={Sproutr}
-                  alt="webapp homescreen snapshot"
-                />
-              </div>
+          {projectCard.map(
+            ({
+              src,
+              textContent: { title, description },
+              button: { codeURL, demoURL },
+            }) => {
+              return (
+                <div className="project">
+                  <div className="content">
+                    <div className="content__photocard">
+                      <img
+                        className="content__photocard-img"
+                        src={src}
+                        alt="webapp homescreen snapshot"
+                      />
+                    </div>
 
-              <div className="content__photocard-container">
-                <div className="content__photocard-content">
-                  <h3>Sproutr</h3>
-                  <p>
-                    Sproutr is a habit building app that lets you choose & track
-                    a new or existing habit. At Sproutr we are about helping
-                    those create the habits that matter.
-                  </p>
-                  <p>
-                    The approach we decided to take was to create a friendly and
-                    visually appealing app to help those making habits fun.
-                  </p>
+                    <div className="content__photocard-container">
+                      <div className="content__photocard-content">
+                        <h3>{title}</h3>
+                        <p>{description}</p>
 
-                  <div className="button">
-                    <a
-                      className="button__link"
-                      href="https://sproutr.netlify.app/"
-                    >
-                      Demo
-                    </a>
-                    <a
-                      className="button__link"
-                      href="https://github.com/CodingClauds/sproutr-capstone"
-                    >
-                      Code
-                    </a>
+                        <div className="button">
+                          <a className="button__link" href={demoURL}>
+                            Demo
+                          </a>
+                          {codeURL && (
+                            <a className="button__link" href={codeURL}>
+                              Code
+                            </a>
+                          )}
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="project">
-            <div className="content">
-              <div className="content__photocard">
-                <img
-                  className="content__photocard-img"
-                  src={Maesurf}
-                  alt="webapp homescreen snapshot"
-                />
-              </div>
-
-              <div className="content__photocard-container">
-                <div className="content__photocard-content">
-                  <h3>MaeSurf</h3>
-                  <p>
-                    MaeSurf is a surf camp website that is designed to help
-                    travellers book full surf camps and lessons in Costa Rica.
-                  </p>
-                  <p>
-                    Currently tasked with organizing the site and adding in key
-                    functionalities to help attract traffic to the site.
-                  </p>
-                  <div className="button">
-                    <a className="button__link" href="https://maesurf.com/">
-                      Demo
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="project">
-            <div className="content">
-              <div className="content__photocard">
-                <img
-                  className="content__photocard-img"
-                  src={Horoscope}
-                  alt="webapp homescreen snapshot"
-                />
-              </div>
-
-              <div className="content__photocard-container">
-                <div className="content__photocard-content">
-                  <h3>Horoscoped</h3>
-                  <p>
-                    For one of our "mini-hackathons" we created a Horoscope app
-                    that utilizes API’s to retrieve data when an individual user
-                    inputs their zodiac sign or their specific birthday.
-                  </p>
-
-                  <br></br>
-
-                  <div className="button">
-                    <a
-                      className="button__link"
-                      href="https://hororscoped.netlify.app/"
-                    >
-                      Demo
-                    </a>
-                    <a
-                      className="button__link"
-                      href="https://github.com/CodingClauds/horoscoped"
-                    >
-                      Code
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="project">
-            <div className="content">
-              <div className="content__photocard">
-                <img
-                  className="content__photocard-img"
-                  src={F1}
-                  alt="webapp homescreen snapshot"
-                />
-              </div>
-
-              <div className="content__photocard-container">
-                <div className="content__photocard-content">
-                  <h3>F1 Stopwatch</h3>
-                  <p>
-                    Here's the Formula-1 themed flipcard game with all 20
-                    drivers from the 2022 driver lineup, try to match the
-                    drivers with their teams before the time runs out! Let's see
-                    if you have what it takes to have a podium finish.
-                  </p>
-
-                  <div className="button">
-                    <a
-                      className="button__link"
-                      href="https://flipcardformula1.netlify.app/"
-                    >
-                      Demo
-                    </a>
-                    <a
-                      className="button__link"
-                      href="https://github.com/CodingClauds/F1-stopwatch"
-                    >
-                      Code
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="project">
-            <div className="content">
-              <div className="content__photocard">
-                <img
-                  className="content__photocard-img"
-                  src={DarkMode}
-                  alt="webapp homescreen snapshot"
-                />
-              </div>
-
-              <div className="content__photocard-container">
-                <div className="content__photocard-content">
-                  <h3>Dark/Light Mode</h3>
-                  <p>
-                    For this challenge, I wanted to implement a light and dark
-                    mode switch toggle that would change the colours or invert
-                    the colours from the default to dark mode for this basic
-                    stopwatch component. The method I used was creating
-                    variables for the two color states, and adding the toggle on
-                    those two throughout.
-                  </p>
-
-                  <div className="button">
-                    <a
-                      className="button__link"
-                      href="https://dark-light-mode-stopwatch.netlify.app/"
-                    >
-                      Demo
-                    </a>
-                    <a
-                      className="button__link"
-                      href="https://github.com/CodingClauds/Dark-light-Mode-StopWatch"
-                    >
-                      Code
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+              );
+            }
+          )}
         </section>
       </div>
     </div>
