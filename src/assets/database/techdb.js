42e5e102d8fb7520dff5d6ccec863889b9bdf5e1
Claudio Miranda <claudio_769@hotmail.com>
2022-10-27 12:19:16 -0400

    Added a mapping function to the techstack section with a db variable

---
+++ src/assets/database/techdb.js
@@ -0,0 +1,23 @@
+import ReactIcon from "../../assets/images/techstack/react.svg";
+import HtmlIcon from "../../assets/images/techstack/html5.svg";
+import CssIcon from "../../assets/images/techstack/css3.svg";
+import SassIcon from "../../assets/images/techstack/sass.svg";
+import JsIcon from "../../assets/images/techstack/javascript.svg";
+import FirebaseIcon from "../../assets/images/techstack/firebase.png";
+import GithubIcon from "../../assets/images/techstack/github.svg";
+import RouterIcon from "../../assets/images/techstack/reactrouter.svg";
+import AxiosIcon from "../../assets/images/techstack/axios.svg";
+import NpmIcon from "../../assets/images/techstack/npm.svg";
+
+export const db = [
+  { src: ReactIcon },
+  { src: HtmlIcon },
+  { src: CssIcon },
+  { src: SassIcon },
+  { src: JsIcon },
+  { src: FirebaseIcon },
+  { src: GithubIcon },
+  { src: RouterIcon },
+  { src: AxiosIcon },
+  { src: NpmIcon },
+];
